0eb22c63cdfd7e3e3abfd3f4f0524f59be274347
Aulon Mujaj <aulon.mujaj@gmail.com>
2020-02-01 22:22:40 +0100

    Added storbyook-docs addon and prettierrc file

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,12 @@
+import 'styleguide/src/'
 module.exports = {
-  stories: ["../stories/**/*.stories.js"],
+  stories: ["../stories/**/*.stories.(js|mdx)"],
   addons: [
     "@storybook/addon-actions/register",
     "@storybook/addon-links/register",
     "@storybook/addon-a11y/register",
     "@storybook/addon-backgrounds/register",
-    "@storybook/addon-knobs/register"
+    "@storybook/addon-knobs/register",
+    "@storybook/addon-docs/register"
   ]
 };
